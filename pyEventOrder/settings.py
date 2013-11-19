7983b1edeb540f6d9c0f90cd11d54e180f4bec1d
Aston May <meihao@live.cn>
2013-11-19 20:56:29 +0800

    修改信息并测试消息返回。

--- pyEventOrder/settings.py
+++ pyEventOrder/settings.py
@@ -52,10 +52,10 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
--- pyEventOrderWeb/messages.py
+++ pyEventOrderWeb/messages.py
@@ -10,6 +10,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 logger = logging.getLogger('django.dev')
+URLBASE = 'http://whitemay.pythonanywhere.com'
 
 
 def processEvent(msg,event):
@@ -25,15 +26,14 @@ def processMessage(msg):
     msg_out['toUser'] =  msg.find('FromUserName').text
     msg_out['fromUser'] = msg.find('ToUserName').text
     msg_out['time'] = int(time.time())
-    msg_out['content'] = 'Hello, Ricky!'
 
-    #t = get_template('textmsg.xml')
-    #output_xml = t.render(Context(msg_out))
-    #logger.debug('Output is ' + output_xml)
+    #msg_out['content'] = 'Hello, Ricky!'
+    #return render_to_response('textmsg.xml', msg_out, content_type='text/xml')
 
-    #msg_out = etree.Element('XML')
-    #output_xml = etree.tostring(msg_out)
-    #logger.debug(output_xml)
-    return render_to_response('textmsg.xml', msg_out, content_type='text/xml')
-    #return HttpResponse(output_xml, content_type='text/xml')
+    article={'title':'发布活动', 'description':'点此链接发布一个活动'}
+    article['picurl'] = URLBASE + '/media/test.png'
+    article['url'] = URLBASE + '/add_event/?user=' + msg_out['toUser']
+
+    msg_out['articles'] = [article]
+    return render_to_response('multimsg.xml', msg_out, content_type='text/xml')
 
