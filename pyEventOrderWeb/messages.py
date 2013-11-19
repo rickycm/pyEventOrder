702a37f54222155442fb9792490cb3ba323a3d1f
Aston May <meihao@live.cn>
2013-11-19 20:10:39 +0800

    修改信息并准备测试消息返回。

--- pyEventOrderWeb/messages.py
+++ pyEventOrderWeb/messages.py
@@ -5,8 +5,9 @@ import logging
 
 from lxml import etree
 from django.http import HttpResponse
-from django.template.loader import get_template
-from django.template import Context
+#from django.template.loader import get_template
+#from django.template import Context
+from django.shortcuts import render_to_response
 
 logger = logging.getLogger('django.dev')
 
@@ -24,13 +25,15 @@ def processMessage(msg):
     msg_out['toUser'] =  msg.find('FromUserName').text
     msg_out['fromUser'] = msg.find('ToUserName').text
     msg_out['time'] = int(time.time())
-    msg_out['content'] = 'Hello, world!'
+    msg_out['content'] = 'Hello, Ricky!'
 
-    t = get_template('textmsg.xml')
-    output_xml = t.render(Context(msg_out))
-    logger.debug('Output is ' + output_xml)
+    #t = get_template('textmsg.xml')
+    #output_xml = t.render(Context(msg_out))
+    #logger.debug('Output is ' + output_xml)
 
     #msg_out = etree.Element('XML')
     #output_xml = etree.tostring(msg_out)
     #logger.debug(output_xml)
-    return HttpResponse(output_xml, content_type='text/xml')
+    return render_to_response('textmsg.xml', msg_out, content_type='text/xml')
+    #return HttpResponse(output_xml, content_type='text/xml')
+
