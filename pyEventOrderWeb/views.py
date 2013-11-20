a0a07f4702215e47c50f82ebbc4b367b154ecdc4
Aston May <meihao@live.cn>
2013-11-20 17:42:34 +0800

    自动保存用户ID信息

--- pyEventOrderWeb/views.py
+++ pyEventOrderWeb/views.py
@@ -209,19 +209,24 @@ def message(request):
 
 from django.http import Http404
 def setting(request):
+    max_age = 0
+    userid = False
     if request.method=='GET':
         if request.COOKIES.has_key('wxopenid'):
             userid = request.COOKIES['wxopenid']
             logger.info('Cookie has userid ' + userid)
-        elif request.GET.has_key('userid'):
+
+        if request.GET.has_key('userid'):
             userid = request.GET['userid']
             logger.debug('Request has userid ' + userid)
-
-            form = forms.EventForm()
-            response = render_to_response('addEvent.html', {'title': '新建活动', 'form': form},
-                                  context_instance=RequestContext(request))
             max_age = 365 * 24 * 60 * 60
-            response.set_cookie("wxopenid", userid, max_age=max_age)
-            return response
-        else:
+
+        if not userid:
             raise Http404
+
+        form = forms.EventForm()
+        response = render_to_response('addEvent.html', {'title': '新建活动', 'form': form},
+            context_instance=RequestContext(request))
+        if max_age>0:
+            response.set_cookie("wxopenid", userid, max_age=max_age)
+        return response
