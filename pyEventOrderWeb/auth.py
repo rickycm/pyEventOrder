e29446d1badedcceb0d394255c921255d9c89993
Aston May <meihao@live.cn>
2013-11-21 21:18:20 +0800

    实现OAuth

--- pyEventOrderWeb/auth.py
+++ pyEventOrderWeb/auth.py
@@ -1,10 +1,12 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class OAuthBackend(object):
 
     def authenticate(self, code):
-        user = User.objects.get(username='user')
+        UserModel = get_user_model()
+        user = UserModel._default_manager.get_by_natural_key('user')
         return user
 
     def get_user(self, user_id):
-        return User.objects.get(pk=user_id)
+        UserModel = get_user_model()
+        return UserModel._default_manager.get_by_natural_key(pk=user_id)
