66686a040a609db19213234090b5c2135191c610
Aston May <meihao@live.cn>
2013-11-25 10:35:15 +0800

    微信账号菜单项

--- TIPS.md
+++ TIPS.md
@@ -1,5 +1,37 @@
 本文档用来保存一些有用的信息
 ==============================
+{
+    "access_token": "SbWY7_g8Z2hVGyC_N9Dqh6sdqnSCakPpoOojQYyvyD3Xc4mk_KjSUAy8RizBJ5-ievh8e9k9EFL8gdAgedssyY7UY1bwQFt8xwQ042d5Wzpslw4njFSp242_a_WvU42PSINMObbM6rZrtX5umEqCkw",
+    "expires_in": 7200
+}
+{
+     "button":[
+     {
+          "type":"click",
+          "name":"发布活动",
+          "key":"NEW_EVENT"
+      },
+      {
+          "name":"活动管理",
+           "sub_button":[
+           {
+               "type":"click",
+               "name":"我发布的活动",
+               "key":"MY_POST"
+            },
+            {
+               "type":"click",
+               "name":"我报名的活动",
+               "key":"MY_EVENT"
+            }]
+      },
+      {
+           "type":"click",
+           "name":"个人信息",
+           "key":"SETTING"
+      }]
+ }
+
 在公众账号中输入set，会触发一条消息，点击进入set页面。
 通过这种方式，服务端会将openid保存到浏览器Cookie内，之后就可以根据这个Cookie来判定是由谁发出的微信访问。
 如果不是通过微信发起的访问，或者是在微信外浏览器中访问，就无法获得相关信息，从而不能工作。
--- pyEventOrderWeb/events.py
+++ pyEventOrderWeb/events.py
@@ -11,9 +11,12 @@ logger = logging.getLogger('django.dev')
 URLBASE = 'http://whitemay.pythonanywhere.com'
 
 def processEvent(msg,event):
-    logger.debug('It is a event: ' + event.text)
-    if event.text=='subscribe':
+    event_type = event.text
+    logger.debug('It is a event: ' + event_type)
+    if event_type=='subscribe':
         # 这是一个订阅消息。
+        # 收到消息后，需要在用户库中做一条记录，并设置为没有设置
+            # 考虑在此申请用户信息。
         userid = msg.find('FromUserName').text
         try:
             user = wechat_user.objects.get(openid=userid)
@@ -23,22 +26,29 @@ def processEvent(msg,event):
             user = wechat_user.objects.create(openid=userid, subscribe=True, initialized=False)
         user.save()
 
-        # 收到消息后，需要在用户库中做一条记录，并设置为没有设置
-            # 考虑在此次申请用户信息。
-        # 然后向用户主动发送一条消息，欢迎用户进入设置页面
-        # 设置页面的URL中间包括用户的userid
-        # 这样用户在打开设置页面时，就可以将userid保存在Cookie中了。
-        msg_out={}
-        msg_out['toUser'] =  userid
-        msg_out['fromUser'] = msg.find('ToUserName').text
-        msg_out['time'] = int(time.time())
+        return sendSetting(userid, msg)
 
-        article={'title':'欢迎', 'description':'使用之前，请点这里设置您的个人信息。'}
-        article['picurl'] = URLBASE + '/media/mask.png'
-        article['url'] = URLBASE + '/setting/?openid=' + userid
-
-        msg_out['articles'] = [article]
-        return render_to_response('multimsg.xml', msg_out, content_type='text/xml')
+    elif event_type=='CLICK':
+        # 目前为止，应该只有SETTING这一个点击项
+        assert msg.find('EventKey').text is 'SETTING'
+        userid = msg.find('FromUserName').text
+        return sendSetting(userid, msg)
 
     raise Http404
 
+def sendSetting(userid, msg):
+    # 然后向用户主动发送一条消息，欢迎用户进入设置页面
+    # 设置页面的URL中间包括用户的userid
+    # 这样用户在打开设置页面时，就可以将userid保存在Cookie中了。
+    msg_out={}
+    msg_out['toUser'] =  userid
+    msg_out['fromUser'] = msg.find('ToUserName').text
+    logger.debug('My id is ', msg_out['fromUser'])
+    msg_out['time'] = int(time.time())
+
+    article={'title':'欢迎', 'description':'使用之前，请点这里设置您的个人信息。'}
+    article['picurl'] = URLBASE + '/media/mask.png'
+    article['url'] = URLBASE + '/setting/?openid=' + userid
+
+    msg_out['articles'] = [article]
+    return render_to_response('multimsg.xml', msg_out, content_type='text/xml')
