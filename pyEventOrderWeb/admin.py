e1b34f2d6fe236e470d858b60bcbdfdef1b1cd28
Aston May <meihao@live.cn>
2013-11-28 14:04:38 +0800

    小修改，准备给别人试用。

--- pyEventOrderWeb/admin.py
+++ pyEventOrderWeb/admin.py
@@ -9,8 +9,8 @@ class EventAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated_date'
 
 class Wechat_userAdmin(admin.ModelAdmin):
-    list_display = ('wechat_fakeID', 'wechat_username', 'wechat_inputname', 'wechat_usertype', 'openid')
-    fields = ('wechat_fakeID', 'wechat_username', 'wechat_inputname', 'wechat_usertype', 'openid')
+    list_display = ('wechat_fakeID', 'wechat_username', 'wechat_inputname', 'wechat_usertype', 'openid', 'initialized')
+    fields = ('wechat_fakeID', 'wechat_username', 'wechat_inputname', 'wechat_usertype', 'openid', 'initialized')
 
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ('event_ID', 'partici_name', 'partici_type', 'partici_openid', 'register_time')
--- pyEventOrderWeb/events.py
+++ pyEventOrderWeb/events.py
@@ -26,7 +26,7 @@ def processEvent(msg,event):
             user = wechat_user.objects.create(openid=userid, subscribe=True, initialized=False)
         user.save()
 
-        return sendSetting(userid, msg)
+        return sendSetting(user, msg)
 
     elif event_type=='CLICK':
         # 目前为止，应该只有SETTING这一个点击项
@@ -34,27 +34,31 @@ def processEvent(msg,event):
         userid = msg.find('FromUserName').text
         logger.debug(userid + 'clicked!')
         try:
-            wechat_user.objects.get(openid=userid)
+            user = wechat_user.objects.get(openid=userid)
         except wechat_user.DoesNotExist:
             user = wechat_user.objects.create(openid=userid, subscribe=True, initialized=False)
             user.save()
-        return sendSetting(userid, msg)
+        return sendSetting(user, msg)
 
     raise Http404
 
-def sendSetting(userid, msg):
+def sendSetting(user, msg):
     # 然后向用户主动发送一条消息，欢迎用户进入设置页面
     # 设置页面的URL中间包括用户的userid
     # 这样用户在打开设置页面时，就可以将userid保存在Cookie中了。
     msg_out={}
-    msg_out['toUser'] =  userid
+    msg_out['toUser'] =  user.openid
     msg_out['fromUser'] = msg.find('ToUserName').text
     logger.debug('My id is ' + msg_out['fromUser'])
     msg_out['time'] = int(time.time())
 
-    article={'title':'欢迎', 'description':'亲，点这里设置您的个人信息，才能正常使用活动功能哦！'}
+    if not user.wechat_inputname:
+        title = '您好，陌生人'
+    else:
+        title = '您好，'+user.wechat_inputname
+    article={'title':title, 'description':'亲，点这里设置您的个人信息，才能正常使用活动功能哦！'}
     article['picurl'] = URLBASE + '/media/badminton.png'
-    article['url'] = URLBASE + '/setting/?openid=' + userid
+    article['url'] = URLBASE + '/setting/?openid=' + user.openid
 
     msg_out['articles'] = [article]
     return render_to_response('multimsg.xml', msg_out, content_type='text/xml')
