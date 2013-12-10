82629dbee814d2e1b07757ace7c724404919d124
Aston May <meihao@live.cn>
2013-12-10 12:00:46 +0800

    实现用户一键注册并登录

--- pyEventOrderWeb/views.py
+++ pyEventOrderWeb/views.py
@@ -773,7 +773,7 @@ def get_wx_info(code, session):
     return jobj
 
 def welcome(request):
-    url = 'http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5NTk2OTU4NA==&appmsgid=10012087&itemidx=1&sign=3f9befba95ade73c3c5f83b594881e4c&uin=MzEzMzUwNQ==&key=234b3ec6051a4a54816adf5cd6fbd4f872b4d614851567edf97b56e5ac6d4f5bdcf6559519f067494d165a04cdc225b6&devicetype=iPhone+OS7.0.4&version=15000311&lang=zh_CN'
+    url = 'http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5NTk2OTU4NA==&appmsgid=10012087&itemidx=1&sign=3f9befba95ade73c3c5f83b594881e4c&uin=MzEzMzUwNQ==&key=234b3ec6051a4a54f91d761ca60cb032bf7deefd4fecf29d9fea504c4cd87ed8acf601210834fda2faf9f7d9a8a79107&devicetype=iPhone+OS7.0.4&version=15000311&lang=zh_CN'
     return HttpResponseRedirect(url)
 
 def welcome_old(request):
